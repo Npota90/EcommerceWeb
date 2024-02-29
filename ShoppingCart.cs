7b05b4085056c43efe967ead7eabd4aea3723d06
Npota90 <158215220+Npota90@users.noreply.github.com>
2024-02-29 18:51:18 +0530

    Add files via upload

---
+++ ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApplication2.Models
+{
+    [Table("ShoppingCart")]
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        [Required]
+        
+        public int UserId { get; set; }
+        public string IsDeleted { get; set; }
+
+
+
+    }
+}
